019cc97033831bd92d48a4637154f76470de3914
fagugon283 <35686794+fagugon283@users.noreply.github.com>
2018-01-24 09:07:23 +0100

    Cambios de prueba

--- server.js
+++ server.js
@@ -1,2 +1,6 @@
-console.log('This example is different!');
-console.log('The result is displayed in the Command Line Interface');
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+ 
+server.listen(server_port, server_ip_address, function () {
+  console.log( "Listening on " + server_ip_address + ", port " + server_port )
+});
